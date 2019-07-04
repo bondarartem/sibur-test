fa78f1b62d29d891ef19037cf02b80bda5ac57c3
Артём <fiolix1995@gmail.com>
2019-07-05 00:02:58 +0700

    Updates

---
+++ precache-manifest.345b4002d4a537130ff68d2a2a6dd247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb1ef2dd0c9328e057700a07be0cb40",
+    "url": "/sibur-test/index.html"
+  },
+  {
+    "revision": "f1b87a79b990cc4a295a",
+    "url": "/sibur-test/static/css/main.4c548a4c.chunk.css"
+  },
+  {
+    "revision": "51b8f2f062d13ce29070",
+    "url": "/sibur-test/static/js/2.209c49bb.chunk.js"
+  },
+  {
+    "revision": "f1b87a79b990cc4a295a",
+    "url": "/sibur-test/static/js/main.dda5b671.chunk.js"
+  },
+  {
+    "revision": "4de3fe71f086b55d35e9",
+    "url": "/sibur-test/static/js/runtime~main.14dc3b4e.js"
+  },
+  {
+    "revision": "99541f31c3aca8a3251b82b28ec7966e",
+    "url": "/sibur-test/static/media/FuturaPTBook.99541f31.otf"
+  },
+  {
+    "revision": "7bc4581342f34758f84baeb53e96b6ba",
+    "url": "/sibur-test/static/media/bg.7bc45813.png"
+  }
+]);
